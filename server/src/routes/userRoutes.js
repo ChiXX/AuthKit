aa1d4d40b639e36b47eb067906c2ef7dda72cc34
Xu CHi <1436160465@qq.com>
2025-06-08 22:13:42 +0200

    register user

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
+import { registerUser, loginUser } from "../controllers/auth/userController.js";
+
 
 const router = express.Router();
 
 
-router.get("/", (req, res) => {
-    res.send("Hello")
-})
-
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 
 export default router;
